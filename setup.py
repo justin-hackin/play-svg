7655a63d4997089b3ded6e6a379c681f29ae647c
imagenerator <cosmo.guffa@gmail.com>
2007-03-31 15:05:51 +0000

    ***

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import os
 import glob
 
 setup(name='playsvg',
-      version='1.0',
+      version='0.1',
       description='playsvg: making graphical programming as easy as py',
       author='imagenerator',
       author_email='touchmewithsynchronicpulses@gmail.com',
       url='http://sourceforge.net/projects/play-svg/',
-      packages=['playsvg'],
-      data_files = [("scripts",[os.path.join("scripts", i) for i in glob.glob(os.path.join("scripts *.py"))] )  ]
+      packages=['playsvg']
+      #data_files = [("scripts",[os.path.join("scripts", i) for i in glob.glob(os.path.join("scripts *.py"))] ), ("", ["README.TXT"]), ("inkex",[os.path.join("inkex", i) for i in glob.glob(os.path.join("inkex *.py"))] )  ]
      )
