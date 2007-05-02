2541f598985b66808177b4ea4e459a424ab22dee
The Playful Geometer <cosmo.guffa@gmail.com>
2007-05-02 15:05:19 +0000

    ***

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ from distutils.core import setup
 import os
 import glob
 
+dataFiles = []
+if os.name == "posix":
+    print "Inkscape extensions will be automatically installed on this Linux-based system"
+    dataFiles.append(("/usr/share/inkscape/extensions/", [i for i in glob.glob(os.path.join("inkex", "*.*"))] ))
+else:
+    print "You are not running Linux, therefore you must install Inkscape extensions manually by copying files in the inkex directory to the Inkscape extensions directory on your system"
+
 setup(name='playsvg',
-      version='0.1',
+      version='0.2',
       description='playsvg: making graphical programming as easy as py',
-      author='imagenerator',
-      author_email='touchmewithsynchronicpulses@gmail.com',
+      author='playful_geometr',
+      author_email='justinbarca@gmail.com',
       url='http://sourceforge.net/projects/play-svg/',
-      packages=['playsvg']
+      packages=['playsvg'],
+      data_files=dataFiles
       #data_files = [("scripts",[os.path.join("scripts", i) for i in glob.glob(os.path.join("scripts *.py"))] ), ("", ["README.TXT"]), ("inkex",[os.path.join("inkex", i) for i in glob.glob(os.path.join("inkex *.py"))] )  ]
      )
